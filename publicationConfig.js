460ebd26dd36c06fa6af9fff55ac67f61cb1b3bb
lishu <lishugupta652@gmail.com>
2022-11-12 19:47:42 +0530

    fix: publications updated

---
+++ publicationConfig.js
@@ -0,0 +1,47 @@
+const publication = [
+  // Jitendra Kumar, Ashutosh Kumar Singh, Anand Mohan, Rajkumar Buyya, ‘‘Machine Learning for Cloud Management’’, Chapman and Hall/CRC , 1st edition, 2021. (ISBN: 9780367626488)
+  {
+    authors: [
+      "Jitendra Kumar",
+      "Ashutosh Kumar Singh",
+      "Anand Mohan",
+      "Rajkumar Buyya",
+    ],
+    title: '"Machine Learning for Cloud Management"',
+    link: "https://www.routledge.com/Machine-Learning-for-Cloud-Management/Kumar-Singh-Mohan-Buyya/p/book/9780367622565",
+    venue: "",
+    publisher: "Chapman and Hall/CRC",
+    otherDetails: "1st edition, 2021. (ISBN: 9780367626488)",
+    highlights: "",
+    category: "book",
+    year: "2021",
+  },
+  // Jitendra Kumar, Ashutosh Kumar Singh, ‘‘Performance evaluation of metaheuristic algorithms for workload prediction in cloud environment’’, Applied Soft Computing, vol. 113, Part A, 2021, pp. 107895. (SCI, Q1, IF= 6.725 , ISSN: 1568-4946)
+  {
+    authors: ["Jitendra Kumar", "Ashutosh Kumar Singh"],
+    title:
+      '"Performance evaluation of metaheuristic algorithms for workload prediction in cloud environment"',
+    link: "https://doi.org/10.1016/j.asoc.2021.107895",
+    venue: "Applied Soft Computing",
+    publisher: "",
+    otherDetails:
+      "vol. 113, Part A, 2021, pp. 107895. (SCI, Q1, IF= 6.725 , ISSN: 1568-4946)",
+    highlights: "",
+    category: "journal",
+    year: "2021",
+  },
+  // Jitendra Kumar, Ashutosh Kumar Singh, ‘‘Cloud resource demand prediction using differential evolution based learning’’, 2019 7th International Conference on Smart Computing & Communications (ICSCC), Sarawak, Malaysia, Malaysia, 2019, pp. 1-5. (Best Paper Award), (Scopus)
+  {
+    authors: ["Jitendra Kumar", "Ashutosh Kumar Singh"],
+    title:
+      '"Cloud resource demand prediction using differential evolution based learning"',
+    link: "https://ieeexplore.ieee.org/document/8843680",
+    venue:
+      "2019 7th International Conference on Smart Computing & Communications (ICSCC)",
+    publisher: "",
+    otherDetails: "Sarawak, Malaysia, Malaysia, 2019, pp. 1-5. , (Scopus)",
+    highlights: "(Best Paper Award)",
+    category: "conference",
+    year: "2019",
+  },
+];
