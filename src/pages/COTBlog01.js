e6dca1ade00eb0d54753e47d338f7d5dafc632f4
lishu <lishugupta652@gmail.com>
2022-11-18 16:41:10 +0530

    fix: publication updates

--- src/pages/COTBlog01.js
+++ src/pages/COTBlog01.js
@@ -4,6 +4,23 @@ import Header from "../components/Header";
 import { Container } from "../theme/global.styled";
 
 const COTBlog01Styled = styled.div`
+  li {
+    margin-left: 1.8rem;
+  }
+  .highlight {
+    color: rgba(127, 17, 70, 1);
+
+    font-weight: bold;
+    font-size: 1.2rem;
+    padding: 5px 10px;
+    margin: 5px 0;
+    margin-top: 35px;
+    background-color: #eaeaea;
+    width: 100%;
+    display: block;
+    border-radius: 3px;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+  }
   h1 {
     margin: 4rem 0 1rem 0;
     text-align: center;
@@ -39,10 +56,6 @@ const COTBlog01Styled = styled.div`
     text-align: justify;
   }
 
-  .topics ol {
-    margin-left: 2rem;
-  }
-
   .importantDates ul {
     list-style: none;
 
@@ -53,19 +66,12 @@ const COTBlog01Styled = styled.div`
   }
 
   .editors ol {
-    margin-left: 5rem;
   }
 
   .questions {
     margin-top: 2rem;
     text-align: center;
     margin-bottom: 3rem;
-  }
-
-  .highlight {
-    color: rgba(127, 17, 70, 1);
-
-    font-weight: bold;
   }
 `;
 
@@ -86,22 +92,32 @@ const COTBlog01 = () => {
           <span className="dateVal"> 31.01.2023</span>
         </p>
 
-        <marquee><h3><a href="https://cmt3.research.microsoft.com/CoT2023" target="_blank" rel="noopener noreferrer">Click here</a> to submit your chapter.</h3></marquee>
-
-        
+        <marquee>
+          <h3>
+            <a
+              href="https://cmt3.research.microsoft.com/CoT2023"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Click here
+            </a>{" "}
+            to submit your chapter.
+          </h3>
+        </marquee>
 
         <div className="para1">
-         
-          Dear Researchers/Academicians, <br/>
-          We invite you to contribute your quality research into our upcoming book to be published by CRC Press-Taylor & Francis Group.<br/>
-          <span className="highlight">Note: </span> 
-        
+          Dear Researchers/Academicians, <br />
+          We invite you to contribute your quality research into our upcoming
+          book to be published by CRC Press-Taylor & Francis Group.
+          <br />
+          <h2 className="highlight">Note </h2>
           <ol>
-            <li>There is NO processing/publication charge for the book chapter.</li>
+            <li>
+              There is NO processing/publication charge for the book chapter.
+            </li>
             <li>The book will be sent for possible indexing in Scopus.</li>
           </ol>
-      
-          <span className="highlight">About the Book:</span> Cloud computing has
+          <h2 className="highlight">About the Book</h2> Cloud computing has
           received a tremendous acceptance across the globe due to its
           characteristics including on-demand service, accessibility,
           reliability, scalability, elasticity, and many others. In modern
@@ -162,8 +178,9 @@ const COTBlog01 = () => {
 
         <div className="topics">
           <div className="topicLine">
-            <span className="highlight">Topics of Interest: </span>The
-            submissions are welcome in the following areas but not limited to:
+            <h2 className="highlight">Topics of Interest </h2>
+            The submissions are welcome in the following areas but not limited
+            to:
           </div>
 
           <ol>
@@ -191,7 +208,7 @@ const COTBlog01 = () => {
         </div>
 
         <div className="importantDates">
-          <div className="highlight">Important Dates</div>
+          <h2 className="highlight">Important Dates</h2>
 
           <ul>
             <li>
@@ -222,62 +239,153 @@ const COTBlog01 = () => {
         </div>
 
         <div className="submission">
-          <div className="highlight">Submission Guidelines</div>
+          <h2 className="highlight">Submission Guidelines</h2>
           <p>
             <ol>
-              <li>All Manuscripts must be original and not simultaneously submitted to another journal or conference. All the submissions should be made only through Microsoft CMT Only. [<a href="https://cmt3.research.microsoft.com/CoT2023" target="_blank" rel="noopener noreferrer">SUBMISSION LINK</a>]</li>
-              <li>Plagiarism Policy: The submitted full chapter must not be submitted elsewhere for any type of consideration for publishing. The Author should ensure the Plagiarism % is less than 15% (from any single source less than 2%).</li>
-              <li>Reviewing Policy: The editor(s) will engage two single-blind peer-reviewers to assess originality, clarity, usefulness, and adherence to the scope of the chapter.</li>
+              <li>
+                All Manuscripts must be original and not simultaneously
+                submitted to another journal or conference. All the submissions
+                should be made only through Microsoft CMT Only. [
+                <a
+                  href="https://cmt3.research.microsoft.com/CoT2023"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  SUBMISSION LINK
+                </a>
+                ]
+              </li>
+              <li>
+                Plagiarism Policy: The submitted full chapter must not be
+                submitted elsewhere for any type of consideration for
+                publishing. The Author should ensure the Plagiarism % is less
+                than 15% (from any single source less than 2%).
+              </li>
+              <li>
+                Reviewing Policy: The editor(s) will engage two single-blind
+                peer-reviewers to assess originality, clarity, usefulness, and
+                adherence to the scope of the chapter.
+              </li>
             </ol>
-
           </p>
-          <div className="highlight">Important Guidelines for Manuscript Preparation</div>
+          <h2 className="highlight">
+            Important Guidelines for Manuscript Preparation
+          </h2>
           <p>
             <ol>
-              <li>The formatting of manuscripts should follow: A4 page format, single column with 11 point Times New Roman font with 1.5 line spacing, while submitting your manuscript as a Microsoft Office Word document. Use the number referencing style is usually used for regular research papers, NOT APA style.</li>
-              <li>Article of 15-20 pages should be submitted for a single chapter.</li>
-              <li>Authors must provide a chapter abstract (no more than 150 words each).</li>
-              <li>Original articles with significant contributions from the Author(s) will be the major consideration for shortlisting the received chapters.</li>
-              <li>We are collaborating with an international publisher, it is mandatory that emphasis should be given for the correct use of English in the chapters. Use online grammar checking tools like ginger or Grammarly.</li>
-              <li>The title of the chapter should be concise, precise and should clearly state the main aim of the chapter. It must correlate to the proposed book chapter titles conveyed during the call of book chapter submission. Editors will arrange the chapters in a sequence so that they must be well synchronized and are in line with the title of the book. Thus, the title of the chapters can be re-framed to go well with the theme of the book, if required.</li>
-              <li>Figures should NOT be copied from different websites or research articles. If they are copied, please add a reference or re-draw the diagrams.</li>
-              <li>If you incorporate in the Work any material from third parties, then you have to take permission in writing from each copyright holder of such material to use and reprint such material in the Work consistent with the terms of the rights.</li>
-              <li>Customized Copyright Form and Permission Verification Form (PVF) will be shared among all authors in the last stage of this book submission to the publisher</li>
+              <li>
+                The formatting of manuscripts should follow: A4 page format,
+                single column with 11 point Times New Roman font with 1.5 line
+                spacing, while submitting your manuscript as a Microsoft Office
+                Word document. Use the number referencing style is usually used
+                for regular research papers, NOT APA style.
+              </li>
+              <li>
+                Article of 15-20 pages should be submitted for a single chapter.
+              </li>
+              <li>
+                Authors must provide a chapter abstract (no more than 150 words
+                each).
+              </li>
+              <li>
+                Original articles with significant contributions from the
+                Author(s) will be the major consideration for shortlisting the
+                received chapters.
+              </li>
+              <li>
+                We are collaborating with an international publisher, it is
+                mandatory that emphasis should be given for the correct use of
+                English in the chapters. Use online grammar checking tools like
+                ginger or Grammarly.
+              </li>
+              <li>
+                The title of the chapter should be concise, precise and should
+                clearly state the main aim of the chapter. It must correlate to
+                the proposed book chapter titles conveyed during the call of
+                book chapter submission. Editors will arrange the chapters in a
+                sequence so that they must be well synchronized and are in line
+                with the title of the book. Thus, the title of the chapters can
+                be re-framed to go well with the theme of the book, if required.
+              </li>
+              <li>
+                Figures should NOT be copied from different websites or research
+                articles. If they are copied, please add a reference or re-draw
+                the diagrams.
+              </li>
+              <li>
+                If you incorporate in the Work any material from third parties,
+                then you have to take permission in writing from each copyright
+                holder of such material to use and reprint such material in the
+                Work consistent with the terms of the rights.
+              </li>
+              <li>
+                Customized Copyright Form and Permission Verification Form (PVF)
+                will be shared among all authors in the last stage of this book
+                submission to the publisher
+              </li>
               <li>Please use templates given below:</li>
             </ol>
-
-            LaTeX: <a href="https://www.overleaf.com/latex/templates/taylor-and-francis-latex-template-for-authors-interact-layout-plus-apa-reference-style/jqhskrsqqzfz" target="_blank" rel="noopener noreferrer">https://www.overleaf.com/latex/templates/taylor-and-francis-latex-template-for-authors-interact-layout-plus-apa-reference-style/jqhskrsqqzfz</a> <br/>
-            Microsoft Office Word: <a href="https://docs.google.com/document/d/1DKIFZAqO2LzzrurzeOFbSDh2qzL_tOo_/edit?usp=share_link&ouid=109550927316957289401&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer">https://docs.google.com/document/d/1DKIFZAqO2LzzrurzeOFbSDh2qzL_tOo_/edit?usp=share_link&ouid=109550927316957289401&rtpof=true&sd=true</a> <br/>
+            LaTeX:
+            <a
+              href="https://www.overleaf.com/latex/templates/taylor-and-francis-latex-template-for-authors-interact-layout-plus-apa-reference-style/jqhskrsqqzfz"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              https://www.overleaf.com/latex/templates/taylor-and-francis-latex-template-for-authors-interact-layout-plus-apa-reference-style/jqhskrsqqzfz
+            </a>{" "}
+            <br />
+            Microsoft Office Word:
+            <a
+              href="https://docs.google.com/document/d/1DKIFZAqO2LzzrurzeOFbSDh2qzL_tOo_/edit?usp=share_link&ouid=109550927316957289401&rtpof=true&sd=true"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              https://docs.google.com/document/d/1DKIFZAqO2LzzrurzeOFbSDh2qzL_tOo_/edit?usp=share_link&ouid=109550927316957289401&rtpof=true&sd=true
+            </a>{" "}
+            <br />
           </p>
         </div>
 
         <div className="editors">
-          <div className="highlight">Editors:</div>
+          <h2 className="highlight">Editors</h2>
 
           <ol className="editor">
             <li>
-              Dr. Jitendra Kumar, National Institute of Technology Tiruchirappalli, India [<a href = "mailto: jitendra@nitt.edu">jitendra@nitt.edu</a>]
+              Dr. Jitendra Kumar, National Institute of Technology
+              Tiruchirappalli, India [
+              <a href="mailto: jitendra@nitt.edu">jitendra@nitt.edu</a>]
             </li>
             <li>
-              Dr. G. R. Gangadharan, National Institute of Technology Tiruchirappalli, India [<a href = "mailto: ganga@nitt.edu">ganga@nitt.edu</a>]
+              Dr. G. R. Gangadharan, National Institute of Technology
+              Tiruchirappalli, India [
+              <a href="mailto: ganga@nitt.edu">ganga@nitt.edu</a>]
             </li>
             <li>
-              Prof. Ashutosh Kumar Singh, National Institute of Technology Kurukshetra, India [<a href = "mailto: ashutosh@nitkkr.ac.in">ashutosh@nitkkr.ac.in</a>]
+              Prof. Ashutosh Kumar Singh, National Institute of Technology
+              Kurukshetra, India [
+              <a href="mailto: ashutosh@nitkkr.ac.in">ashutosh@nitkkr.ac.in</a>]
             </li>
             <li>
-              Prof. Chung-Nan Lee, National Sun Yat-sen University, Kaohsiung, Taiwan [<a href = "mailto: cnlee@mail.cse.nsysu.edu.tw">cnlee@mail.cse.nsysu.edu.tw</a>]
+              Prof. Chung-Nan Lee, National Sun Yat-sen University, Kaohsiung,
+              Taiwan [
+              <a href="mailto: cnlee@mail.cse.nsysu.edu.tw">
+                cnlee@mail.cse.nsysu.edu.tw
+              </a>
+              ]
             </li>
           </ol>
         </div>
 
         <div className="publisher">
-          <span className="highlight">Publisher: </span>
+          <h2 className="highlight">Publisher </h2>
           <span className="publisherName">CRC Publications</span>
         </div>
 
         <div className="questions">
           <div className="highlight">Questions?</div>
-          <p><a href = "mailto: cot23crc@gmail.com">Contact Us</a></p>
+          <p>
+            <a href="mailto: cot23crc@gmail.com">Contact Us</a>
+          </p>
         </div>
       </Container>
     </COTBlog01Styled>
