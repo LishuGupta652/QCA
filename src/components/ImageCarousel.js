583063991c1f59860b2d3d29b0b3cc4e86c22ccd
lishu <lishugupta652@gmail.com>
2022-11-21 22:39:33 +0530

    fix: cosmetic changes

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -17,6 +17,9 @@ const ImageCarousel = () => {
         autoPlay
         showThumbs={false}
         onSwipeMove
+        emulateTouch
+        infiniteLoop
+        swipeable
       >
         <div>
           <img src="https://images.unsplash.com/photo-1668881233694-1825a663b2a4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
