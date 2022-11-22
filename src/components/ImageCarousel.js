cd74d8235fb19e40a19d6bc6152d834ec12dd591
Dr. Jitendra Kumar <52519832+jitendrakv@users.noreply.github.com>
2022-11-22 05:21:36 +0000

    recent research image path updated

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -22,16 +22,16 @@ const ImageCarousel = () => {
         swipeable
       >
         <div>
-          <img src="https://images.unsplash.com/photo-1668881233694-1825a663b2a4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
-          <p className="legend">Cloud</p>
+          <img src="/workspaces/QCA/src/img/recentResearch/eqnn1.jpg" />
+          <p className="legend">Quantum Approach to Forecast Cloud Workloads</p>
         </div>
         <div>
-          <img src="https://images.unsplash.com/photo-1668881233694-1825a663b2a4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
-          <p className="legend">Cloud</p>
+          <img src="/workspaces/QCA/src/img/recentResearch/eqnn2.jpg" />
+          <p className="legend">Quantum Approach to Forecast Cloud Workloads</p>
         </div>
         <div>
-          <img src="https://images.unsplash.com/photo-1668881233694-1825a663b2a4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
-          <p className="legend">Cloud</p>
+          <img src="/workspaces/QCA/src/img/recentResearch/eqnn3.jpg" />
+          <p className="legend">Quantum Approach to Forecast Cloud Workloads</p>
         </div>
       </Carousel>
     </ImageCarouselStyled>
