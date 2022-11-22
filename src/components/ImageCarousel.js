675a818a92964c82adf28c7a66e0f0b45d26a791
Dr. Jitendra Kumar <52519832+jitendrakv@users.noreply.github.com>
2022-11-22 08:37:13 +0000

    recent research img update

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -30,39 +30,15 @@ const ImageCarousel = () => {
         swipeable
       >
         <div>
-          <img src={require("../img/recentResearch/eqnn1.jpg")} alt="" />
+          <img src={require("../img/recentResearch/FT.jpg")}  />
           <p className="legend"><a href="https://ieeexplore.ieee.org/document/9428529" target="_blank" rel="noopener noreferrer">Quantum Approach to Forecast Cloud Workloads</a></p>
         </div>
         <div>
-          <img src={require("../img/recentResearch/eqnn2.jpg")}  />
-          <p className="legend"><a href="https://ieeexplore.ieee.org/document/9428529" target="_blank" rel="noopener noreferrer">Quantum Approach to Forecast Cloud Workloads</a></p>
-        </div>
-        <div>
-          <img src={require("../img/recentResearch/eqnn3.jpg")}  />
-          <p className="legend"><a href="https://ieeexplore.ieee.org/document/9428529" target="_blank" rel="noopener noreferrer">Quantum Approach to Forecast Cloud Workloads</a></p>
-        </div>
-        <div>
-          <img src={require("../img/recentResearch/ETERM (1).png")}  />
+          <img src={require("../img/recentResearch/FT.png")}  />
           <p className="legend"><a href="https://ieeexplore.ieee.org/document/9891787" target="_blank" rel="noopener noreferrer">Fault-Tolerant Elastic Resource Management</a></p>
         </div>
         <div>
-          <img src={require("../img/recentResearch/ETERM (2).png")}  />
-          <p className="legend"><a href="https://ieeexplore.ieee.org/document/9891787" target="_blank" rel="noopener noreferrer">Fault-Tolerant Elastic Resource Management</a></p>
-        </div>
-        <div>
-          <img src={require("../img/recentResearch/ETERM (3).png")}  />
-          <p className="legend"><a href="https://ieeexplore.ieee.org/document/9891787" target="_blank" rel="noopener noreferrer">Fault-Tolerant Elastic Resource Management</a></p>
-        </div>
-        <div>
-          <img src={require("../img/recentResearch/HAMM (1).jpg")}  />
-          <p className="legend"><a href="https://doi.org/10.1109/TSC.2022.3206417" target="_blank" rel="noopener noreferrer">Significance Rank-Driven High Availability</a></p>
-        </div>
-        <div>
-          <img src={require("../img/recentResearch/HAMM (2).jpg")}  />
-          <p className="legend"><a href="https://doi.org/10.1109/TSC.2022.3206417" target="_blank" rel="noopener noreferrer">Significance Rank-Driven High Availability</a></p>
-        </div>
-        <div>
-          <img src={require("../img/recentResearch/HAMM (3).jpg")}  />
+          <img src={require("../img/recentResearch/FT.jpg")}  />
           <p className="legend"><a href="https://doi.org/10.1109/TSC.2022.3206417" target="_blank" rel="noopener noreferrer">Significance Rank-Driven High Availability</a></p>
         </div>
       </Carousel>
