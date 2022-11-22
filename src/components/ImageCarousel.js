c7539b71631b9e1abaf1de0593525314f2dcf55e
Dr. Jitendra Kumar <52519832+jitendrakv@users.noreply.github.com>
2022-11-22 06:40:22 +0000

    research paper links

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -41,6 +41,30 @@ const ImageCarousel = () => {
           <img src={require("../img/recentResearch/eqnn3.jpg")}  />
           <p className="legend"><a href="https://ieeexplore.ieee.org/document/9428529" target="_blank" rel="noopener noreferrer">Quantum Approach to Forecast Cloud Workloads</a></p>
         </div>
+        <div>
+          <img src={require("../img/recentResearch/ETERM (1).jpg")}  />
+          <p className="legend"><a href="https://ieeexplore.ieee.org/document/9891787" target="_blank" rel="noopener noreferrer">Fault-Tolerant Elastic Resource Management</a></p>
+        </div>
+        <div>
+          <img src={require("../img/recentResearch/ETERM (2).jpg")}  />
+          <p className="legend"><a href="https://ieeexplore.ieee.org/document/9891787" target="_blank" rel="noopener noreferrer">Fault-Tolerant Elastic Resource Management</a></p>
+        </div>
+        <div>
+          <img src={require("../img/recentResearch/ETERM (3).jpg")}  />
+          <p className="legend"><a href="https://ieeexplore.ieee.org/document/9891787" target="_blank" rel="noopener noreferrer">Fault-Tolerant Elastic Resource Management</a></p>
+        </div>
+        <div>
+          <img src={require("../img/recentResearch/HAMM (1).jpg")}  />
+          <p className="legend"><a href="https://doi.org/10.1109/TSC.2022.3206417" target="_blank" rel="noopener noreferrer">Significance Rank-Driven High Availability</a></p>
+        </div>
+        <div>
+          <img src={require("../img/recentResearch/HAMM (2).jpg")}  />
+          <p className="legend"><a href="https://doi.org/10.1109/TSC.2022.3206417" target="_blank" rel="noopener noreferrer">Significance Rank-Driven High Availability</a></p>
+        </div>
+        <div>
+          <img src={require("../img/recentResearch/HAMM (3).jpg")}  />
+          <p className="legend"><a href="https://doi.org/10.1109/TSC.2022.3206417" target="_blank" rel="noopener noreferrer">Significance Rank-Driven High Availability</a></p>
+        </div>
       </Carousel>
     </ImageCarouselStyled>
   );
