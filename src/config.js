7ad1a7d891d6a3d845cad8b1516b892ab5a5dbc9
lishu <lishugupta652@gmail.com>
2022-12-18 09:26:08 +0530

    fix: added journals

--- src/config.js
+++ src/config.js
@@ -446,4 +446,98 @@ export const publicationsConfig = [
     category: "book chapter",
     year: "2016",
   },
+  // Ashutosh Kumar Singh, Sakshi Chhabra, Rishabh Gupta, and Deepika Saxena, “A Reliable Client Detection System during Load Balancing for Multi-tenant Cloud Environments”, SN Computer Science, 2022 (SCOPUS).
+  {
+    authors: [
+      "Ashutosh Kumar Singh",
+      "Sakshi Chhabra",
+      "Rishabh Gupta",
+      "Deepika Saxena",
+    ],
+    title:
+      '"A Reliable Client Detection System during Load Balancing for Multi-tenant Cloud Environments"',
+    link: "",
+    venue: "",
+    publisher: "SN Computer Science",
+    otherDetails: "",
+    highlights: "",
+    category: "journal",
+    year: "2022",
+  },
+
+  // Rishabh Gupta, Ishu Gupta, Deepika Saxena, Ashutosh Kumar Singh, Chung-Nan Lee, “An IoT-Centric Data Protection Method for Preserving Security and Privacy in Cloud”, IEEE Systems Journal, (SCI, impact factor = 3.931).
+  {
+    authors: [
+      "Rishabh Gupta",
+      "Ishu Gupta",
+      "Deepika Saxena",
+      "Ashutosh Kumar Singh",
+      "Chung-Nan Lee",
+    ],
+    title:
+      '"An IoT-Centric Data Protection Method for Preserving Security and Privacy in Cloud"',
+    link: "",
+    venue: "",
+    publisher: "IEEE Systems Journal",
+    otherDetails: "",
+    highlights: "",
+    category: "journal",
+    year: "2022",
+  },
+
+  // Ashutosh Kumar Singh, Rishabh Gupta, and Deepika Saxena, “Differential and TriPhase adaptive learning-based Privacy-Preserving Model for Medical Data in Cloud Environment”, IEEE Networking Letters, (SCI, impact factor = 2.6).
+  {
+    authors: ["Ashutosh Kumar Singh", "Rishabh Gupta", "Deepika Saxena"],
+    title:
+      '"Differential and TriPhase adaptive learning-based Privacy-Preserving Model for Medical Data in Cloud Environment"',
+    link: "",
+    venue: "",
+    publisher: "Networking Letters",
+    otherDetails: "",
+    highlights: "",
+    category: "journal",
+    year: "2022",
+  },
+
+  // Ashutosh Kumar Singh and Smruti Rekha Swain and Chung Nan Lee, “A Metaheuristic Virtual Machine Placement Framework towards Power Efficiency of Sustainable Cloud Environment”, Journal of Soft Computing, Springer (SCI, impact factor = 3.732)
+  {
+    authors: ["Ashutosh Kumar Singh", "Smruti Rekha", "Chung Nan Lee"],
+    title:
+      '"A Metaheuristic Virtual Machine Placement Framework towards Power Efficiency of Sustainable Cloud Environment"',
+    link: "",
+    venue: "",
+    publisher: "Journal of Soft Computing",
+    otherDetails: "",
+    highlights: "",
+    category: "journal",
+    year: "2022",
+  },
+
+  // Deepika Saxena, Ashutosh Kumar Singh, “A High Availability Management Model based on VM Significance Ranking and Resource Estimation for Cloud Applications”, IEEE Transactions on Services Computing, 2022 (SCI, impact factor = 11.019)
+  {
+    authors: ["Deepika Saxena", "Ashutosh Kumar Singh"],
+    title:
+      '"A High Availability Management Model based on VM Significance Ranking and Resource Estimation for Cloud Applications"',
+    link: "",
+    venue: "",
+    publisher: "IEEE Transactions on Services Computing",
+    otherDetails: "",
+    highlights: "",
+    category: "journal",
+    year: "2022",
+  },
+
+  // Ashutosh Kumar Singh and Jatinder Kumar, “A Privacy-Preserving Multidimensional Data Aggregation Scheme with Secure Query Processing for Smart Grid”, The Journal of Supercomputing, (SCI, impact factor = 2.557)
+  {
+    authors: ["Ashutosh Kumar Singh", "Jatinder Kumar"],
+    title:
+      '"A Privacy-Preserving Multidimensional Data Aggregation Scheme with Secure Query Processing for Smart Grid"',
+    link: "",
+    venue: "",
+    publisher: "The Journal of Supercomputing",
+    otherDetails: "",
+    highlights: "",
+    category: "journal",
+    year: "2022",
+  },
 ];
