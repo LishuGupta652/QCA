5378b35179ef4a68d7d1eccbc7cfb7f2974a7e35
Dr. Jitendra Kumar <52519832+jitendrakv@users.noreply.github.com>
2023-10-11 18:51:21 +0000

    research projects added

--- src/pages/Research.js
+++ src/pages/Research.js
@@ -17,10 +17,28 @@ const Research = () => {
 
         <h2>Research Projects</h2>
         <ol>
-          <li> Defence Research and Development Organization Funding - 
-              Intelligent Real Time Situation Awareness and Detection Support System for Indian Defence  [2016-2018]
+          <li> Funded by Defence Research and Development Organization, Govt. of India - 
+            Intelligent Real Time Situation Awareness and Detection Support System for Indian Defence  [2016-2018]
+          </li>
+          <li>Funded by Minister of Science, Technology and Innovation of Malaysia, Malaysia - 
+            Adaptive Low Power System-on-Chip Architectures [2012-2014].
+          </li>
+          <li>Funded by Curtin Sarawak Collaborative Research, Curtin University of Technology, Malaysia - 
+          Design and Implementation of Power Efficient Algorithm for Information Signal Processing [2009-2012] 
+          </li>
+          <li>
+          Funded by CSRF, Curtin University of Technology, Malaysia - An Efficient Logic Synthesis Approach with Testability Using MODD [2008-2009]
+          </li>
+          <li>
+          Funded by Multimedia University, Malaysia - Application of Decision Diagrams in Synthesis, Design and Testing of VLSI [2003-2004]
           </li>
         </ol>
+
+        
+
+
+
+
       </Container>
       <Footer />
     </>
