6428d3215599ae93a08552a2a6e5e23a08bcb529
lishu <lishugupta652@gmail.com>
2023-10-16 16:38:08 +0530

    feat: change in title

--- src/components/Home/HomeAbout.js
+++ src/components/Home/HomeAbout.js
@@ -4,9 +4,9 @@ import { APP_NAME, HEADLINE_1 } from "../../config";
 const HomeAbout = () => {
   return (
     <div className="home_about">
-      <h1>
+      {/* <h1>
         Welcome to <span className="app_name">{APP_NAME} </span>
-      </h1>
+      </h1> */}
 
       <p>{HEADLINE_1}</p>
     </div>
