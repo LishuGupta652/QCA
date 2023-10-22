1e66f2db69173c462f4ca554b74a73122c7f51b1
lishu <lishugupta652@gmail.com>
2023-10-22 19:20:59 +0530

    feat: research Projects

--- src/pages/Research.js
+++ src/pages/Research.js
@@ -1,8 +1,67 @@
 import React from "react";
+import styled from "styled-components";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import ResearchCard from "../components/Home/ResearchCard";
 import { Container } from "../theme/global.styled";
+
+const ResearchProjectCard = styled.div`
+  width: 95%;
+  margin: 0 auto;
+  min-height: calc(100vh - 200px);
+
+  .projectCard {
+    background-color: rgba(34, 34, 34, 0.1);
+    margin: 10px 0;
+    padding: 10px;
+    border-radius: 5px;
+    color: #454545;
+    margin-top: 30px;
+    display: flex;
+    flex-direction: column;
+    user-select: none;
+  }
+`;
+
+const ResearchProjects = [
+  {
+    title:
+      "Theoretical Investigation and Analysis of Different Architectures of Quantum Computing for Cyber Physical Systems",
+    fundingAgency: "MPCST, Govt. of Madhya Pradesh",
+    duration: "2023-2025",
+  },
+  {
+    title:
+      "Intelligent Real Time Situation Awareness and Detection Support System for Indian Defence",
+    fundingAgency:
+      "Defence Research and Development Organization, Govt. of India",
+    duration: "2016-2018",
+  },
+  {
+    title: "Adaptive Low Power System-on-Chip Architectures",
+    fundingAgency: "Minister of Science, Technology and Innovation of Malaysia",
+    duration: "2012-2014",
+  },
+  {
+    title:
+      "Design and Implementation of Power Efficient Algorithm for Information Signal Processing",
+    fundingAgency:
+      "Curtin Sarawak Collaborative Research, Curtin University of Technology, Malaysia",
+    duration: "2009-2012",
+  },
+  {
+    title: "An Efficient Logic Synthesis Approach with Testability Using MODD",
+    fundingAgency:
+      "Curtin Sarawak Research Fund, Curtin University of Technology, Malaysia",
+    duration: "2008-2009",
+  },
+  {
+    title:
+      "Application of Decision Diagrams in Synthesis, Design and Testing of VLSI",
+    fundingAgency: "Multimedia University, Malaysia",
+    duration: "2003-2004",
+  },
+];
 
 const Research = ({ isTitleCenter }) => {
   React.useEffect(() => {
@@ -15,39 +74,21 @@ const Research = ({ isTitleCenter }) => {
       <Container>
         <ResearchCard isTitleCenter />
 
-        <h2>Research Projects</h2>
-        <ol>
-          <li>
-            Funded by MPCST, Govt. of Madhya Pradesh - Theoretical Investigation
-            and Analysis of Different Architectures of Quantum Computing for
-            Cyber Physical Systems [2023-2025]
-          </li>
-          <li>
-            {" "}
-            Funded by Defence Research and Development Organization, Govt. of
-            India - Intelligent Real Time Situation Awareness and Detection
-            Support System for Indian Defence [2016-2018]
-          </li>
-          <li>
-            Funded by Minister of Science, Technology and Innovation of
-            Malaysia, Malaysia - Adaptive Low Power System-on-Chip Architectures
-            [2012-2014].
-          </li>
-          <li>
-            Funded by Curtin Sarawak Collaborative Research, Curtin University
-            of Technology, Malaysia - Design and Implementation of Power
-            Efficient Algorithm for Information Signal Processing [2009-2012]
-          </li>
-          <li>
-            Funded by CSRF, Curtin University of Technology, Malaysia - An
-            Efficient Logic Synthesis Approach with Testability Using MODD
-            [2008-2009]
-          </li>
-          <li>
-            Funded by Multimedia University, Malaysia - Application of Decision
-            Diagrams in Synthesis, Design and Testing of VLSI [2003-2004]
-          </li>
-        </ol>
+        <ResearchProjectCard>
+          <h2>Research Projects</h2>
+          <ol>
+            {ResearchProjects.map((project, index) => (
+              <li key={project.title}>
+                <div className="projectCard">
+                  {project.title}
+                  <br />
+                  <span>{project.fundingAgency}</span>
+                  <span>{project.duration}</span>
+                </div>
+              </li>
+            ))}
+          </ol>
+        </ResearchProjectCard>
       </Container>
       <Footer />
     </>
